4dd9cabe7eab52f96ac40e0ac5c759a42484f432
shamsutdin_islamov <Shamsutdin_Islamov@student.itpu.uz>
2025-01-28 10:04:57 +0500

    registered user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,7 +6,7 @@ export const register = (req, res) => {
     
     const q = "SELECT * FROM users WHERE email = ?";
 
-    db.query(q, [req.body.username], (err, data) => {
+    db.query(q, [req.body.email], (err, data) => {
         if (err) return res.status(500).json(err);
 
         if (data.length) return res.status(409).json("User already exists");
